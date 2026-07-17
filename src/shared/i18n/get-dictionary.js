35c18967ddafc616299096c4253977d67c27f0fe
ErickAbanto <aeriatab@gmail.com>
2026-07-16 20:13:51 -0500

    feat: implement modular services page with multilingual support and localized content dictionaries

--- src/shared/i18n/get-dictionary.js
+++ src/shared/i18n/get-dictionary.js
@@ -5,13 +5,15 @@ const dictionaries = {
     const common = await import('./dictionaries/common/en.json').then((m) => m.default).catch(() => ({}));
     const home = await import('./dictionaries/home/en.json').then((m) => m.default).catch(() => ({}));
     const sobreNosotros = await import('./dictionaries/sobre-nosotros/en.json').then((m) => m.default).catch(() => ({}));
-    return { navbar: common.navbar, home, sobreNosotros, errors: common.errors };
+    const servicios = await import('./dictionaries/servicios/en.json').then((m) => m.default).catch(() => ({}));
+    return { navbar: common.navbar, home, sobreNosotros, servicios, errors: common.errors };
   },
   es: async () => {
     const common = await import('./dictionaries/common/es.json').then((m) => m.default).catch(() => ({}));
     const home = await import('./dictionaries/home/es.json').then((m) => m.default).catch(() => ({}));
     const sobreNosotros = await import('./dictionaries/sobre-nosotros/es.json').then((m) => m.default).catch(() => ({}));
-    return { navbar: common.navbar, home, sobreNosotros, errors: common.errors };
+    const servicios = await import('./dictionaries/servicios/es.json').then((m) => m.default).catch(() => ({}));
+    return { navbar: common.navbar, home, sobreNosotros, servicios, errors: common.errors };
   },
 };
 
